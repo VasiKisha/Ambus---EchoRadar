efceec69076af6edf7304687d562bfdf19c2a63e
VasiKisha <71970981+VasiKisha@users.noreply.github.com>
2020-09-28 15:45:17 +0200

    Update EchoRadar.ino

--- EchoRadar/EchoRadar.ino
+++ EchoRadar/EchoRadar.ino
@@ -1,3 +1,9 @@
+//Copyright (c) 2020 VasiKisha
+//All rights reserved.
+
+//This source code is licensed under the MIT-style license found in the
+//LICENSE file in the root directory of this source tree. 
+
 //Device description
 #define DEVICE "Echo Radar"
 //Firmware version
